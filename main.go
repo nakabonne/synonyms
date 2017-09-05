6ba402af99313d88a33ea552a20745494740f71d
ryonakao <rodriguez.nak@gmail.com>
2017-09-05 12:21:17 +0900

    Add processing stdIn

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"thesaurus"
+)
+
+func main() {
+	apiKey := os.Getenv("BHT_APIKEY")
+	thesaurus := &thesaurus.BigHuge{APIKey: apiKey}
+	s := bufio.NewScanner(os.Stdin)
+}
